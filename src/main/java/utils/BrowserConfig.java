24e4616056416aa7a3d2062192df2c962f2385a8
Keneth <keneth@mobile22.com>
2021-08-16 20:30:29 -0400

    Adding drivers for MAC OS

--- src/main/java/utils/BrowserConfig.java
+++ src/main/java/utils/BrowserConfig.java
@@ -7,38 +7,50 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
 public class BrowserConfig {
-    private static WebDriver driver;
+	private static WebDriver driver;
+	private static final String path = "./src/main/resources/drivers/";
 
-    public static WebDriver getChromerDriver() {
+	public static WebDriver getChromerDriver() {
 
-        System.setProperty("webdriver.chrome.driver", "./src/main/resources/drivers/chromedriver.exe");
-        System.out.println("Opening Google CHROME...");
+		if (System.getProperty("os.name").contains("Windows")) {
+			System.setProperty("webdriver.chrome.driver", path + "Windows/chromedriver.exe");
+		} else {
+			System.setProperty("webdriver.chrome.driver", path + "Mac/chromedriver");
+		}
+
+		System.out.println("Opening Google CHROME...");
 		ChromeOptions opt = new ChromeOptions();
 		opt.setAcceptInsecureCerts(true);
 		driver = new ChromeDriver(opt);
-        return driver;
-    }
+		return driver;
+	}
 
-    public static WebDriver getEdgeDriver() {
-        System.setProperty("webdriver.edge.driver", "./src/main/resources/drivers/msedgedriver.exe");
-        System.out.println("Opening Microsoft EDGE...");
-        EdgeOptions opt = new EdgeOptions();
-        opt.setCapability("acceptSslCerts", true);
-        driver = new EdgeDriver(opt);
-        return driver;
-    }
+	public static WebDriver getEdgeDriver() {
+		
+		if (System.getProperty("os.name").contains("Windows")) {
+			System.setProperty("webdriver.edge.driver", path + "Windows/msedgedriver.exe");
+		} else {
+			System.setProperty("webdriver.edge.driver", path + "Mac/msedgedriver");
+		}
+		
+		System.out.println("Opening Microsoft EDGE...");
+		EdgeOptions opt = new EdgeOptions();
+		opt.setCapability("acceptSslCerts", true);
+		driver = new EdgeDriver(opt);
+		return driver;
+	}
 
-    public static WebDriver getDriver() {
-        if (driver == null) {
-            driver = getEdgeDriver();
-            System.out.println("Driver is NULL. Using EDGE as default option");
-        }
-        return driver;
-    }
+	public static WebDriver getDriver() {
+		if (driver == null) {
+			driver = getEdgeDriver();
+			System.out.println("Driver is NULL. Using EDGE as default option");
+		}
+		return driver;
+	}
 
-    public static void closeDriver() {
-        System.out.println("Closing driver...");
-        driver.quit();
-    }
+	public static void closeDriver() {
+		System.out.println("Closing driver...");
+		driver.quit();
+	}
 
 }
